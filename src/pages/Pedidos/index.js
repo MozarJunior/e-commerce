f1d46edd8a25742ab01b8602a9b962f40adf69e8
Mozar Junior <mozarjunior97@gmail.com>
2023-07-05 19:42:52 -0300

    Inicio da área de Pedidos

--- src/pages/Pedidos/index.js
+++ src/pages/Pedidos/index.js
@@ -1,22 +1,188 @@
-import React, { useEffect } from "react";
-import { SafeAreaView, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, Text, View, TouchableOpacity } from 'react-native';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles from "./style";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../../components/config";
+import { getAuth } from "firebase/auth";
+import { useIsFocused } from "@react-navigation/native";
+import { FlatList } from "react-native-web";
 
 export default function Pedidos(){
+    const isFocused = useIsFocused();
+    const [produtos, setProdutos] = useState([]);
+    const [pedidos, setPedidos] = useState([]);
+    const [usuario, setUsuario]  = useState([]);
+    const [usuario_id, setUsuario_id] = useState('')
+    const [user_id, setUser_id] = useState('')
+
+    const generalPedidos = async () => {
+        await authUser();
+        const ped = []
+        getDocs(query(collection(db, 'pedidos'), where('usuario_id', '==', usuario_id))).then((docSnap) => {
+            docSnap.forEach((doc) => {
+                pedidos.push(doc.data().produto_id);
+            })
+        })
+
+        pedidos.forEach((docSnap => {
+            getDocs(collection(db, 'produto')).then(docSnapPro => {
+                if(docSnap == docSnapPro.id){
+                    produtos.push({...docSnapPro.data(), id: docSnapPro.id});
+                }
+            })
+        }))
+
+        // setPedidos(ped)
+    }
 
     useEffect (() => {
-        
-    });
+        generalPedidos();
+    }, []);
+
+    // useEffect(() => {
+    //     generalProdutos();
+    // })
+
+    useEffect(() => {
+        if(isFocused){
+            console.log('A pagina foi recarregada');
+            generalPedidos();
+        }
+    }, [isFocused]);
+
+    useEffect(() => {
+        if(pedidos.length == 0){
+            generalPedidos();
+            console.log("pesquisando produtos")
+        }else{
+            console.log(pedidos);
+        }
+    }, [])
+    // useEffect(() => {
+    //     if(produtos.length == 0){
+    //         generalProdutos();
+    //         console.log("pesquisando produtos")
+    //     }else{
+    //         console.log(produtos);
+    //     }
+    // }, [])
+
+    const auth = async () => {
+        const auth = getAuth();
+        const user = auth.currentUser
+
+        if(user) {
+            const userUid = user.uid;
+            setUser_id(userUid);
+        }
+    }
+
+    const authUser = async () => {
+        try {
+            await auth();
+            const tabela = collection(db, 'usuario');
+            const q = query(tabela, where('user_id', '==', user_id))
+            const snapShot = await getDocs(q);
+
+            const user = []
+            if(usuario.length < 1){
+                snapShot.forEach((doc) => {
+                    setUsuario(doc.data());
+                    setUsuario_id(doc.id);
+                })
+            }
+        } catch (error) {
+            console.log("Erro ao consultar coleção usuario: ", error)
+        }
+    }
+
+    useEffect(() => {
+        auth();
+        authUser();
+    }, [])
 
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.homeHeader}>
                 <Text style={styles.homeHeaderText}>Meus Pedidos</Text>
             </View>
+            <View style={styles.homeCard}>
+
+                <View style={styles.sectionCategoria}>
+                    <View style={styles.bodyCardIcon}>
+                        <TouchableOpacity style={styles.cardIcon}>
+                            <FontAwesome5 name="box" color={"#00aaff"} size={35}/>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.textCardIcon}>Todos</Text>
+                </View>
+                
+                <View style={styles.sectionCategoria}>
+                    <View style={styles.bodyCardIcon}>
+                        <TouchableOpacity style={styles.cardIcon} onPress={() => writeProdutos()}>
+                            <FontAwesome name="dollar" color={"#00aaff"} size={35}/>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.textCardIcon}>Aguardando pagamento</Text>
+                </View>
+                
+                <View style={styles.sectionCategoria}>
+                    <View style={styles.bodyCardIcon}>
+                        <TouchableOpacity style={styles.cardIcon} onPress={() => bookProdutos()}>
+                            <MaterialCommunityIcons name="truck" color={"#00aaff"} size={40}/>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.textCardIcon}>A caminho</Text>
+                </View>
+                <View style={styles.sectionCategoria}>
+                    <View style={styles.bodyCardIcon}>  
+                        <TouchableOpacity style={styles.cardIcon} onPress={() => accessoryProdutos()}>
+                            <MaterialCommunityIcons name="truck-check" color={"#00aaff"} size={40}/>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.textCardIcon}>Entregue</Text>
+                </View>
+                
+            </View>
 
             <View style={styles.bodyProduct}>
 
             </View>
+            {/* <View style={styles.bodyProduct}>
+                <FlatList
+                    style={styles.bodyFlat}
+                    showsVerticalScrollIndicator={false}
+                    data={produtos}
+                    renderItem={(item) => {
+                        return (
+                            <Pressable style={styles.cardProduct} onPress={() => props.navigation.navigate('details', {
+                                produto_id: item.item.id
+                            })}>
+                                <Image style={styles.cardImage} source={{ uri: item.item.imagem }}/>
+                                <View style={styles.cardBody}>
+                                    <View style={styles.section01}>
+                                        <Text style={styles.cardNameProduto}>{ item.item.nome.length > 20 ? item.item.nome.substring(0, 20).toUpperCase() + '...' : item.item.nome.toUpperCase() }</Text>
+                                        <View style={styles.sectionPreco}>
+                                            <Text style={styles.cardPrecoProduto}>{ item.item.preco } R$</Text>
+                                            <Text style={styles.cardQuantProduto}>Restam {item.item.quantidade} pcs</Text>
+                                        </View>
+                                    </View>
+                                    <TouchableOpacity style={styles.cardAction} onPress={() => deleteCart(item.item.id)}>
+                                        <AntDesign name="delete" color={'#fff'} size={20}/>
+                                    </TouchableOpacity>
+                                </View>
+                                
+                            </Pressable>
+                        );
+                    } }
+                />
+            </View> */}
 
         </SafeAreaView>
     );
--- src/pages/Pedidos/style.js
+++ src/pages/Pedidos/style.js
@@ -120,6 +120,133 @@ const styles = StyleSheet.create({
         fontWeight: 400,
         paddingTop: 5,
     },
+
+    //Inicio do Card de Status
+
+    homeCard: {
+        height: '30%',
+        width: '95%',
+        marginVertical: 20,
+        backgroundColor: '#fff',
+        alignItems: "center",
+        justifyContent: "space-around",
+        flexDirection: 'row',
+        borderRadius: 20,
+    },
+
+    cardIcon: {
+        height: '90%',
+        width: '90%',
+        borderRadius: 5,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: "center",
+    },
+
+    textCardIcon: {
+        fontSize: 11,
+    },
+
+    bodyCardIcon: {
+        height: '100%',
+        width: '100%',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+        elevation: 3,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    sectionCategoria: {
+        height: '35%',
+        width: '20%',
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    //Fim do card de Status
+
+    bodyFlat: {
+        flex: 1,
+        height: '100%',
+        width: '100%',
+    },
+
+    cardProduct: {
+        height: 110,
+        width: '90%',
+        marginVertical: 5,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        flexDirection: 'row',
+    },
+
+    cardBody: {
+        height: '100%',
+        width: '80%',
+        alignItems: "center",
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        borderRadius: 10,
+        flexDirection: 'row',
+
+    },
+
+    cardImage: {
+        height: '100%',
+        width: '35%',
+        borderRadius: 10,
+    },
+
+    cardNameProduto: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        justifyContent: 'flex-start',
+        alignContent: 'flex-start',
+        paddingTop: 5,
+        paddingBottom: 10,
+        paddingHorizontal: 10,
+    },
+
+    sectionPreco:{
+        flexDirection: 'column',
+        height: '50%',
+        width: '80%',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+    },
+
+    section01: {
+        width: '80%',
+        height: '100%',
+    },  
+
+    cardAction: {
+        backgroundColor: '#ff6b6b',
+        height: '100%',
+        width: '20%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    cardPrecoProduto: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#00aaff',
+        paddingTop: 5,
+    },
+    
+    cardQuantProduto: {
+        fontSize: 14,
+        fontWeight: 500,
+        paddingTop: 5,
+        color: '#000',
+    },
 });
 
 export default styles;
